461f2679ba44582089fb01b01f3c2f6d5950465c
ShamxxxD <leethong1997@gmail.com>
2023-03-01 23:02:40 +0700

    refactor source

--- server/src/config/database/database.ts
+++ server/src/config/database/database.ts
@@ -1,16 +1,12 @@
 import mongoose from 'mongoose';
 
-const { DB_USERNAME, DB_PASSWORD } = process.env;
-const DB_URI = `mongodb+srv://shamdev2811:Thong281197@cluster0.kmq0ffu.mongodb.net/test`;
-
-// const DB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.kmq0ffu.mongodb.net/test`;
 const connectDatabase = async () => {
-      try {
-            await mongoose.connect(DB_URI);
-            console.log('Database connected!');
-      } catch (error) {
-            console.log(error);
-      }
+  try {
+    await mongoose.connect(process.env.DB_URI as string);
+    console.log('Database connected!');
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { connectDatabase };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-
 import { connectDatabase } from './config/database/database';
 import route from './routes/index';
+require('dotenv').config(); // Add this line to use dotenv
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT ?? 3000;
 
 connectDatabase();
 app.use(bodyParser.json());
@@ -15,5 +16,5 @@ app.use(cors());
 route(app);
 
 app.listen(port, () => {
-      console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
